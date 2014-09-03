8fe919139d057afbaf58d63aa6662d213f03a4a5
ahmet <ahmet.kakici@pro-line.com.tr>
2014-09-03 14:40:56 +0300

    namespaces

--- ProPictureBox.cs
+++ ProPictureBox.cs
@@ -1,4 +1,8 @@
-﻿public struct ProTransformation
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+public struct ProTransformation
 {
     public Point Translation { get { return _translation; } }
     public double Scale { get { return _scale; } }
